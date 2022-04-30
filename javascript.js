3d48c8198e5ea138297d723198c37e22e4d9d1e2
Alex <18722853@student.westernsydney.edu.au>
2022-04-30 17:58:15 +1000

    created computerPlay function

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+//Return the computers choice of rock, scissors or paper
+function computerPlay() {
+    let options = ["scissors", "paper", "rock"];
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+const computerSelection = computerPlay();
+console.log(computerSelection);
